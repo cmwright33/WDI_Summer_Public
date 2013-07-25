daa946a10b0189eadd0893baa050bf1040be089d
Christopher Wright <cmwright33@gmail.com>
2013-07-25 13:46:43 -0400

    colsure intro

--- examples/js_closures/js/closure_intro.js
+++ examples/js_closures/js/closure_intro.js
@@ -1,18 +1,19 @@
 function handleStudents(){
   var i = 0;  // scope: local variable, type: number
-  var students = ['Jim', 'Bob', 'Tracy', 'Justin', 'Emily'];  // s: local, t:???
-  return function(){  // from SO: the inner function has access to the 
-    // environment it was created within 
+  var students = ['Jim', 'Bob', 'Tracy', 'Justin', 'Emily'];  // s: local, t:Array
+  return function(){  // from SO: the inner function has access to the
+    // environment it was created within
     // even after the code that created the environment has returned.
-    var student =  students[i]; // s: ???, t: string
-    i+=1; 
-    return student;// What type is this returning? 
+    var student =  students[i]; // s: local, t: string
+    i+=1;
+    return student;// What type is this returning? String
   };
 }
 
 // What type of variable is nextStudent?
-// t: ???
-var nextStudent = handleStudents(); 
+// t: Function
+var nextStudent = handleStudents();
+var nextStudent1 = handleStudents();
 
 // onload
 window.onload = function(){
@@ -26,6 +27,21 @@ window.onload = function(){
   };
 
   document.getElementById("resetClosure").onclick = function(){
-    nextStudent = handleStudents(); 
+    nextStudent = handleStudents();
+  };
+
+
+
+
+  document.getElementById("addStudent1").onclick = function(){
+    document.getElementById('students').innerHTML += nextStudent1() + '<br/>\n';
+  };
+
+  document.getElementById("clearStudents1").onclick = function(){
+    document.getElementById('students').innerHTML = '';
+  };
+
+  document.getElementById("resetClosure1").onclick = function(){
+    nextStudent1 = handleStudents();
   };
 };
