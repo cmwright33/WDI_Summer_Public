80b2828e18542e82b11c86c4f694a4c9dc8b1827
Christopher Wright <cmwright33@gmail.com>
2013-08-06 12:00:03 -0400

    guitar runner funner

--- assignments/testing_in_rspec_jasmine/javascript_tests_to_code/src/guitar.js
+++ assignments/testing_in_rspec_jasmine/javascript_tests_to_code/src/guitar.js
@@ -6,33 +6,82 @@ if (typeof Object.create !== 'function') {
 	};
 }
 
+var new_guitar = Object.create(guitar);
+
 var guitar = {
+
+	name: "The Best",
 	numberOfStrings: 6,
 	volume: 5,
-	name: "Fender Esquire",
+
 	setName: function(name) {
 		this.name = name;
 	},
-	breakString: function() {
-		this.numberOfStrings = this.numberOfStrings - 1;
-		if (this.numberOfStrings < 1) {
-			this.volume = 0;
+
+	breakString: function(numberOfStrings) {
+		this.numberOfStrings = parseInt(numberOfStrings) - 1
+			if (numberOfStrings < 1) {
+				guitar.volume = 0;
 		}
-	},
+	};
+
 	restring: function() {
-		this.numberOfStrings = 6;
-		this.volume = 5;
-	},
-	playRock: function() {
-		this.numberOfStrings > 0 ? this.volume = 11 : this.volume = 0;
-	},
-	restringForMoreRock: function() {
-		this.numberOfStrings = 7;
-	},
-	playBasicRhythm: function () {
-		return "jug jigga jug jigga jug";
-	},
-	playHighStrings: function () {
-		return "meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley MEEEEEEEEEEEEEEEE. \'And the dragon comes in the NIIIiiiiIIIiiiiIIIIIIIIiiiiiiiiiiiIIIIIIIIiiiIIGGHH\'";
-	}
+		guitar.numberOfStrings = 6;
+		guitar.volume = 5;
+	};
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	numberOfStrings: 6,
+// 	volume: 5,
+// 	name: "Fender Esquire",
+// 	setName: function(name) {
+// 		this.name = name;
+// 	},
+// 	breakString: function() {
+// 		this.numberOfStrings = this.numberOfStrings - 1;
+// 		if (this.numberOfStrings < 1) {
+// 			this.volume = 0;
+// 		}
+// 	},
+// 	restring: function() {
+// 		this.numberOfStrings = 6;
+// 		this.volume = 5;
+// 	},
+// 	playRock: function() {
+// 		this.numberOfStrings > 0 ? this.volume = 11 : this.volume = 0;
+// 	},
+// 	restringForMoreRock: function() {
+// 		this.numberOfStrings = 7;
+// 	},
+// 	playBasicRhythm: function () {
+// 		return "jug jigga jug jigga jug";
+// 	},
+// 	playHighStrings: function () {
+// 		return "meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley MEEEEEEEEEEEEEEEE. \'And the dragon comes in the NIIIiiiiIIIiiiiIIIIIIIIiiiiiiiiiiiIIIIIIIIiiiIIGGHH\'";
+// 	}
+// };
