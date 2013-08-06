bafacf6dc0ac99fb6d45bc1f73ff41802d2a1d9d
Christopher Wright <cmwright33@gmail.com>
2013-08-06 11:22:53 -0400

    this is a commit

--- assignments/testing_in_rspec_jasmine/ruby_code_to_tests/spec/bank_spec.rb
+++ assignments/testing_in_rspec_jasmine/ruby_code_to_tests/spec/bank_spec.rb
@@ -2,4 +2,52 @@ require_relative 'spec_helper'
 require_relative '../bank'
 
 describe Bank do
+  let(:bank) {Bank.new('New Bank')}
+
+  describe ".new" do
+    it "creates a Bank object" do
+      expect(bank).to_not eq nil
+    end
+    it "had no accounts" do
+      expect(bank.accounts.count).to eq 0
+    end
+  end
+
+  describe "#name" do
+    it "the Bank has a name" do
+      expect(bank.name).to eq 'New Bank'
+    end
+  end
+
+  describe "#create_account" do
+    it "creates an account"do
+    bank.create_account('John Smith', 400)
+    expect(bank.accounts['John Smith']).to eq 400
+    expect(bank.accounts.count).to eq 1
+  end
+end
+  describe "#deposit" do
+    it "deposits money into an account" do
+    bank.create_account('John Smith', 400)
+    bank.deposit('John Smith', 50)
+    expect(bank.accounts['John Smith']).to eq 450
+    end
+  end
+
+  describe "#withdraw" do
+    it "withdraws money from a account" do
+      bank.create_account('John Smith', 400)
+      bank.withdraw('John Smith', 50)
+      expect(bank.accounts['John Smith']).to eq 350
+    end
+  end
+
+  describe "#balance" do
+    it "checks the balance" do
+      bank.create_account('John Smith', 400)
+      bank.balance('John Smith')
+      expect(bank.balance('John Smith')).to eq 400
+    end
+  end
+
 end
--- assignments/testing_in_rspec_jasmine/ruby_tests_to_code/school.rb
+++ assignments/testing_in_rspec_jasmine/ruby_tests_to_code/school.rb
@@ -1,3 +1,29 @@
 class School
   attr_accessor :name, :students
+
+  def initialize(name)
+    @name = name
+    @students = {}
+  end
+
+  def enroll_student(name, gpa)
+
+    @students[name] = gpa
+
+  end
+
+  def grade(name, gpa)
+
+    @students[name] = gpa
+
+  end
+
+  def average_gpa
+    students.values.reduce(:+).to_f / @students.count
+  end
+
+  def withdraw_student(name)
+    @students[name].delete
+  end
+
 end
